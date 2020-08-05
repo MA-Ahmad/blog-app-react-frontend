146d2296afcef500c6392e780d0b69c87c228e56
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-05 09:31:31 -0700

    Initial setup

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Header from "../shared/Header";
+import Form from "./Form";
+import Home from "./Home";
+
+const Main = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route exact path="/new" component={Form} />
+        <Redirect to="/home" />
+      </Switch>
+    </React.Fragment>
+  );
+};
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { theme, ThemeProvider, CSSReset } from "@chakra-ui/core";
+
+const breakpoints = ["360px", "768px", "1024px", "1440px"];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+const newTheme = {
+  ...theme,
+  breakpoints
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={newTheme}>
+      <CSSReset />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+// function App() {
+//   return (
+//     <ThemeProvider theme={newTheme}>
+//       <CSSReset />
+//       <Header />
+//       <App />
+//     </ThemeProvider>
+//   );
+// }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
