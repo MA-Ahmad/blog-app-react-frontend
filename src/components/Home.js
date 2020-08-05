8d4eee8d491ccec5d6584d7e8dd76fb96d8620d3
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-05 10:17:21 -0700

    home page updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,199 @@
 import React from "react";
+import { Box, Image, Badge, Flex, SimpleGrid } from "@chakra-ui/core";
+import Dotdotdot from "react-dotdotdot";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box
+      maxWidth="1200px"
+      mx="auto"
+      my="auto"
+      paddingTop="20px"
+      paddingBottom="20px"
+    >
+      <SimpleGrid columns={3} spacing="15px">
+        <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
+          <Image src="https://bit.ly/2Z4KKcF" alt="Blog image" />
+
+          <Box p="6">
+            <Box d="flex" alignItems="baseline">
+              <Badge rounded="full" px="2" variantColor="teal">
+                New
+              </Badge>
+              <Box
+                fontWeight="semibold"
+                as="h2"
+                letterSpacing="wide"
+                textTransform="uppercase"
+                ml="2"
+              >
+                Hi I'm title of this blog
+              </Box>
+            </Box>
+
+            <Dotdotdot clamp={3}>
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="p"
+                lineHeight="tight"
+                color="gray.600"
+                fontSize="sm"
+              >
+                Modern home in city center in the heart of historic Los Angeles
+                sfsfsdf sdfsdf sdfsdfsf Modern home in city center in the heart
+                of historic Los Angeles sfsfsdf sdfsdf sdfsdfsf
+              </Box>
+            </Dotdotdot>
+          </Box>
+        </Box>
+
+        <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
+          <Image src="https://bit.ly/2Z4KKcF" alt="Blog image" />
+
+          <Box p="6">
+            <Box d="flex" alignItems="baseline">
+              <Badge rounded="full" px="2" variantColor="teal">
+                New
+              </Badge>
+              <Box
+                fontWeight="semibold"
+                as="h2"
+                letterSpacing="wide"
+                textTransform="uppercase"
+                ml="2"
+              >
+                Hi I'm title of this blog
+              </Box>
+            </Box>
+
+            <Dotdotdot clamp={3}>
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="p"
+                lineHeight="tight"
+                color="gray.600"
+                fontSize="sm"
+              >
+                Modern home in city center in the heart of historic Los Angeles
+                sfsfsdf sdfsdf sdfsdfsf Modern home in city center in the heart
+                of historic Los Angeles sfsfsdf sdfsdf sdfsdfsf
+              </Box>
+            </Dotdotdot>
+          </Box>
+        </Box>
+
+        <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
+          <Image src="https://bit.ly/2Z4KKcF" alt="Blog image" />
+
+          <Box p="6">
+            <Box d="flex" alignItems="baseline">
+              <Badge rounded="full" px="2" variantColor="teal">
+                New
+              </Badge>
+              <Box
+                fontWeight="semibold"
+                as="h2"
+                letterSpacing="wide"
+                textTransform="uppercase"
+                ml="2"
+              >
+                Hi I'm title of this blog
+              </Box>
+            </Box>
+
+            <Dotdotdot clamp={3}>
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="p"
+                lineHeight="tight"
+                color="gray.600"
+                fontSize="sm"
+              >
+                Modern home in city center in the heart of historic Los Angeles
+                sfsfsdf sdfsdf sdfsdfsf Modern home in city center in the heart
+                of historic Los Angeles sfsfsdf sdfsdf sdfsdfsf
+              </Box>
+            </Dotdotdot>
+          </Box>
+        </Box>
+
+        <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
+          <Image src="https://bit.ly/2Z4KKcF" alt="Blog image" />
+
+          <Box p="6">
+            <Box d="flex" alignItems="baseline">
+              <Badge rounded="full" px="2" variantColor="teal">
+                New
+              </Badge>
+              <Box
+                fontWeight="semibold"
+                as="h2"
+                letterSpacing="wide"
+                textTransform="uppercase"
+                ml="2"
+              >
+                Hi I'm title of this blog
+              </Box>
+            </Box>
+
+            <Dotdotdot clamp={3}>
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="p"
+                lineHeight="tight"
+                color="gray.600"
+                fontSize="sm"
+              >
+                Modern home in city center in the heart of historic Los Angeles
+                sfsfsdf sdfsdf sdfsdfsf Modern home in city center in the heart
+                of historic Los Angeles sfsfsdf sdfsdf sdfsdfsf
+              </Box>
+            </Dotdotdot>
+          </Box>
+        </Box>
+
+        <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
+          <Image src="https://bit.ly/2Z4KKcF" alt="Blog image" />
+
+          <Box p="6">
+            <Box d="flex" alignItems="baseline">
+              <Badge rounded="full" px="2" variantColor="teal">
+                New
+              </Badge>
+              <Box
+                fontWeight="semibold"
+                as="h2"
+                letterSpacing="wide"
+                textTransform="uppercase"
+                ml="2"
+              >
+                Hi I'm title of this blog
+              </Box>
+            </Box>
+
+            <Dotdotdot clamp={3}>
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="p"
+                lineHeight="tight"
+                color="gray.600"
+                fontSize="sm"
+              >
+                Modern home in city center in the heart of historic Los Angeles
+                sfsfsdf sdfsdf sdfsdfsf Modern home in city center in the heart
+                of historic Los Angeles sfsfsdf sdfsdf sdfsdfsf
+              </Box>
+            </Dotdotdot>
+          </Box>
+        </Box>
+      </SimpleGrid>
+    </Box>
+  );
 };
 
 export default Home;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { Box } from "@chakra-ui/core";
 import Header from "../shared/Header";
 import Form from "./Form";
 import Home from "./Home";
@@ -8,11 +9,13 @@ const Main = () => {
   return (
     <React.Fragment>
       <Header />
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route exact path="/new" component={Form} />
-        <Redirect to="/home" />
-      </Switch>
+      <Box width="100vw" minHeight={"100vh"} bg="gray.100">
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route exact path="/new" component={Form} />
+          <Redirect to="/home" />
+        </Switch>
+      </Box>
     </React.Fragment>
   );
 };
