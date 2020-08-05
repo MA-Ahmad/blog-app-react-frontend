413c03fa4b6c36d196a233da167a3572ce499a37
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-05 13:54:50 -0700

    initial form validations using formik

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,9 +9,11 @@ import {
   Textarea,
   Button,
   Heading,
-  useToast
+  useToast,
+  FormErrorMessage
 } from "@chakra-ui/core";
 import BlogContext from "../context/blog-context";
+import { Formik, Field, ErrorMessage, useFormik } from "formik";
 
 const Form = () => {
   const context = useContext(BlogContext);
@@ -50,6 +52,39 @@ const Form = () => {
     setContent(e.target.value);
   };
 
+  const validate = values => {
+    const errors = {};
+    if (!values.title) {
+      errors.title = "Required";
+    } else if (values.title.length > 15) {
+      errors.title = "Must be 15 characters or less";
+    }
+
+    if (!values.authorName) {
+      errors.authorName = "Required";
+    } else if (values.authorName.length > 20) {
+      errors.authorName = "Must be 20 characters or less";
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      title: title,
+      authorName: authorName
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+    onChange: value => {
+      console.log(value);
+      setTitle(title);
+      setAuthorName(authorName);
+    }
+  });
+
   return (
     <Box
       maxWidth="1200px"
@@ -66,41 +101,60 @@ const Form = () => {
 
         <Box p={5} shadow="md" borderWidth="1px" rounded="md" width={"40%"}>
           <Stack isInline spacing={8} align="center">
-            <form onSubmit={handleSubmit} style={{ width: "100%" }}>
-              <FormControl padding="15px" width={"100%"}>
-                <Box paddingBottom={3}>
-                  <FormLabel htmlFor="email">Title</FormLabel>
-                  <Input
-                    type="text"
-                    id="title"
-                    name="title"
-                    value={title}
-                    onChange={handleTitleChange}
-                  />
-                </Box>
-                <Box paddingBottom={3}>
-                  <FormLabel htmlFor="author">Author</FormLabel>
-                  <Input
-                    type="text"
-                    id="author"
-                    name="author"
-                    value={authorName}
-                    onChange={handleNameChange}
-                  />
-                </Box>
-                <Box paddingBottom={5}>
-                  <FormLabel htmlFor="content">Content</FormLabel>
-                  <Textarea
-                    height={"20vh"}
-                    name="content"
-                    value={content}
-                    onChange={handleContentChange}
-                  />
-                </Box>
-                <Button variantColor="teal" float="right" type="submit">
-                  Create
-                </Button>
-              </FormControl>
+            <form onSubmit={formik.handleSubmit} style={{ width: "100%" }}>
+              <Box paddingBottom={3}>
+                {/* <Field name="title" validate={validateTitle}>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.title && form.touched.title}
+                    >
+                      <FormLabel htmlFor="title">Title</FormLabel>
+                      <Input
+                        {...field}
+                        id="title"
+                        placeholder="title"
+                        value={title}
+                        onChange={handleTitleChange}
+                      />
+                      <FormErrorMessage>{form.errors.title}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field> */}
+                <FormLabel htmlFor="title">Title</FormLabel>
+                <Input
+                  type="text"
+                  id="title"
+                  name="title"
+                  value={formik.values.title}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.title ? <div>{formik.errors.title}</div> : null}
+              </Box>
+              <Box paddingBottom={3}>
+                <FormLabel htmlFor="authorName">Author</FormLabel>
+                <Input
+                  type="text"
+                  id="authorName"
+                  name="authorName"
+                  value={formik.values.authorName}
+                  onChange={formik.handleChange}
+                />
+                {formik.errors.authorName ? (
+                  <div>{formik.errors.authorName}</div>
+                ) : null}
+              </Box>
+              <Box paddingBottom={5}>
+                <FormLabel htmlFor="content">Content</FormLabel>
+                <Textarea
+                  height={"20vh"}
+                  name="content"
+                  value={content}
+                  onChange={handleContentChange}
+                />
+              </Box>
+              <Button variantColor="teal" float="right" type="submit">
+                Create
+              </Button>
             </form>
           </Stack>
         </Box>
