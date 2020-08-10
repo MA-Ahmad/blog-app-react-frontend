19d123450e1a8b373ac6cb9c45bafbffaf3bb09a
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-09 23:24:11 -0700

    update redirect route path

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,7 +24,7 @@ const Main = () => {
             return <BlogForm {...props} editMode={true} />;
           }}
         />
-        <Redirect to="/blogs" />
+        <Redirect to="/" />
       </Switch>
     </React.Fragment>
   );
