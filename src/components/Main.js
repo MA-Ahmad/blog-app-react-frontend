0020f17f54bce356c4c425020da541723bf8c50c
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-06 08:15:45 -0700

    Update main.js with authentication

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Header from "../shared/Header";
 import BlogForm from "./BlogForm";
 import Home from "./Home";
+import Register from "./Auth/Register";
+import AuthForm from "./Auth/AuthForm";
+import BlogContext from "../context/blog-context";
 
 const Main = () => {
+  const context = useContext(BlogContext);
+
   return (
     <React.Fragment>
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        {context.isAuth ? (
+          <>
+            {" "}
+            <Route
+              exact
+              path="/new"
+              render={props => {
+                return <BlogForm {...props} editMode={false} />;
+              }}
+            />
+            <Route
+              exact
+              path="/edit/:id"
+              render={props => {
+                return <BlogForm {...props} editMode={true} />;
+              }}
+            />
+          </>
+        ) : (
+          ""
+        )}
+
         <Route
           exact
-          path="/new"
+          path="/register"
           render={props => {
-            return <BlogForm {...props} editMode={false} />;
+            return <AuthForm {...props} formType={"signup"} />;
           }}
         />
         <Route
           exact
-          path="/edit/:id"
+          path="/login"
           render={props => {
-            return <BlogForm {...props} editMode={true} />;
+            return <AuthForm {...props} formType={"login"} />;
           }}
         />
         <Redirect to="/" />
