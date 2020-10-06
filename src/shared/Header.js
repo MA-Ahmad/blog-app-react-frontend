a28c6d81e80acaf93ef4d44ee4527b922dd48191
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-06 08:16:46 -0700

    Update Header.js

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Box, Heading, Flex, Button, Link } from "@chakra-ui/core";
 import { NavLink } from "react-router-dom";
 import { FaGithub } from "react-icons/fa";
 import "../App.css";
+import BlogContext from "../context/blog-context";
 
 // Note: This code could be better, so I'd recommend you to understand how I solved and you could write yours better :)
 const Header = props => {
   const [show, setShow] = React.useState(false);
   const handleToggle = () => setShow(!show);
+  const context = useContext(BlogContext);
 
   return (
     <Flex
@@ -20,8 +22,10 @@ const Header = props => {
       color="white"
     >
       <Flex align="center" mr={5}>
-        <Heading as="h1" size="lg" letterSpacing={"-.1rem"} color="gray.600">
-          Blog
+        <Heading as="h1" size="lg" letterSpacing={"-.1rem"}>
+          <NavLink exact to="/" activeClassName="active">
+            Blogs
+          </NavLink>
         </Heading>
       </Flex>
 
@@ -43,32 +47,80 @@ const Header = props => {
         alignItems="center"
         flexGrow={1}
       >
-        <Box>
-          <Heading fontSize="20px">
-            <NavLink to="/new" activeClassName="active">
-              New
-            </NavLink>
-          </Heading>
-        </Box>
-        <Box ml={3}>
+        {context.isAuth ? (
+          <Box>
+            <Heading fontSize="20px">
+              <NavLink to="/new" activeClassName="active">
+                New
+              </NavLink>
+            </Heading>
+          </Box>
+        ) : (
+          ""
+        )}
+
+        {/* <Box ml={{ sm: "0", md: "3" }}>
           <Heading fontSize="20px">
             <NavLink exact to="/" activeClassName="active">
               List
             </NavLink>
           </Heading>
-        </Box>
+        </Box> */}
       </Box>
 
       <Box
-        display={{ sm: show ? "block" : "none", md: "block" }}
+        display={{ sm: show ? "block" : "none", md: "flex" }}
+        alignItems="center"
         mt={{ base: 4, md: 0 }}
       >
+        <Box
+          display={{ sm: show ? "block" : "none", md: "flex" }}
+          width={{ sm: "full", md: "auto" }}
+          alignItems="center"
+          flexGrow={1}
+          mr={5}
+        >
+          {context.isAuth ? (
+            <Box ml={{ sm: "0", md: "3" }}>
+              <Heading
+                fontSize="20px"
+                onClick={() => context.logout()}
+                cursor="pointer"
+              >
+                Sign Out
+              </Heading>
+            </Box>
+          ) : (
+            <>
+              <Box ml={{ sm: "0", md: "3" }}>
+                <Heading fontSize="20px">
+                  <NavLink exact to="/login" activeClassName="active">
+                    Sign In
+                  </NavLink>
+                </Heading>
+              </Box>
+              <Box ml={{ sm: "0", md: "3" }}>
+                <Heading fontSize="20px">
+                  <NavLink exact to="/register" activeClassName="active">
+                    Sign Up
+                  </NavLink>
+                </Heading>
+              </Box>
+            </>
+          )}
+        </Box>
         <Link
           href="https://github.com/MA-Ahmad/blog-app-react-frontend"
           isExternal
           style={{ textDecoration: "none" }}
+          _hover={{ color: "black" }}
         >
-          <Button leftIcon={FaGithub} bg="transparent" border="1px">
+          <Button
+            leftIcon={FaGithub}
+            bg="transparent"
+            border="1px"
+            mt={{ sm: "2", md: "0" }}
+          >
             View Source
           </Button>
         </Link>
