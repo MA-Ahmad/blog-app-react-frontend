ab756f22c3aed2d6fc9d516ecf0c80897c953921
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-08 14:47:30 -0700

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -18,17 +18,30 @@ const AuthProvider = props => {
   }, []);
 
   const authenticateUser = () => {
-    Authenticate(baseUrl)
+    // Authenticate(baseUrl)
+    //   .then(response => {
+    //     if (response.isAuth) {
+    //       setIsAuth(response.isAuth);
+    //       setUser(response.user);
+    //     } else {
+    //       setIsAuth(false);
+    //     }
+    //     console.log(response);
+    //   })
+    //   .catch(err => console.log(err));
+
+    fetch(`${baseUrl}/sessions/logged_in`, { credentials: "include" })
+      .then(response => response.json())
       .then(response => {
-        if (response.isAuth) {
-          setIsAuth(response.isAuth);
+        console.log(response);
+
+        if (response.logged_in) {
+          setIsAuth(response.logged_in);
           setUser(response.user);
         } else {
           setIsAuth(false);
         }
-        console.log(response);
-      })
-      .catch(err => console.log(err));
+      });
   };
 
   const updateUser = (user, file, history) => {
@@ -96,35 +109,74 @@ const AuthProvider = props => {
       .catch(err => console.log(err));
   };
 
+  // const loginUser = (user, history) => {
+  //   axios
+  //     .post(
+  //       `${baseUrl}/sessions`,
+  //       {
+  //         user: user
+  //       },
+  //       { withCredentials: true }
+  //     )
+  //     .then(response => {
+  //       let message = "",
+  //         status = "";
+  //       if (response.data.logged_in) {
+  //         setIsAuth(true);
+  //         setUser(response.data.user);
+  //         history.push("/");
+  //         // window.location.href = "/";
+  //         message = "Logged in successfully";
+  //         status = "success";
+  //       } else {
+  //         message = "Credentials are wrong";
+  //         status = "error";
+  //       }
+  //       showToast(message, status);
+  //     })
+  //     .catch(error => {
+  //       console.log("login error", error);
+  //     });
+  // };
+
   const loginUser = (user, history) => {
-    axios
-      .post(
-        `${baseUrl}/sessions`,
-        {
-          user: user
-        },
-        { withCredentials: true }
-      )
-      .then(response => {
-        let message = "",
-          status = "";
-        if (response.data.logged_in) {
-          setIsAuth(true);
-          setUser(response.data.user);
-          history.push("/");
-          // window.location.href = "/";
-          message = "Logged in successfully";
-          status = "success";
-        } else {
-          message = "Credentials are wrong";
-          status = "error";
-        }
-        showToast(message, status);
-      })
-      .catch(error => {
-        console.log("login error", error);
-      });
+    postData({ user: user }).then(data => {
+      console.log(data); // JSON data parsed by `data.json()` call
+      let message = "",
+        status = "";
+      if (data.logged_in) {
+        setIsAuth(true);
+        setUser(data.user);
+        history.push("/");
+        // window.location.href = "/";
+        message = "Logged in successfully";
+        status = "success";
+      } else {
+        message = "Credentials are wrong";
+        status = "error";
+      }
+      showToast(message, status);
+    });
   };
+
+  // Example POST method implementation:
+  async function postData(data = {}) {
+    // Default options are marked with *
+    const response = await fetch(`${baseUrl}/sessions`, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json"
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
 
   const showToast = (message, status) => {
     toast({
