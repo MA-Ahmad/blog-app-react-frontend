a90e6665348628b773af2cf5c372181799f20d32
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-08 04:45:07 -0700

    Create ProtectedRoute.js

---
+++ src/ProtectedRoute.js
@@ -0,0 +1,19 @@
+// based off this code sand box: https://codesandbox.io/s/p71pr7jn50
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+
+// Redirect users away from these routes if they are NOT logged in
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const context = useContext(AuthContext);
+  return (
+    <Route
+      render={props =>
+        context.isAuth ? <Component {...props} /> : <Redirect to="/" />
+      }
+      {...rest}
+    />
+  );
+};
+
+export default ProtectedRoute;
