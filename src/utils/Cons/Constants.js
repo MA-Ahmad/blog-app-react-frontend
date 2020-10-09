62a3aa7fe0fbeaa8b0afddcc5523a2ad8dfee2ab
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-09 08:58:37 -0700

    Update Constants.js

--- src/utils/Cons/Constants.js
+++ src/utils/Cons/Constants.js
@@ -1,4 +1,4 @@
-// export const apiHost = "http://localhost:3001/api/v1";
 export const apiHost = "https://blog-backend-rails.herokuapp.com/api/v1";
-// export const baseUrl = "http://localhost:3001";
 export const baseUrl = "https://blog-backend-rails.herokuapp.com";
+// export const apiHost = "http://localhost:3001/api/v1";
+// export const baseUrl = "http://localhost:3001";
