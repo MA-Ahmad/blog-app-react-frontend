882da3bfd816e00877b9e19e54670a38cbf2a7bd
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-15 06:06:17 -0700

    Add default colors

--- src/App.js
+++ src/App.js
@@ -3,16 +3,47 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./components/Main";
 import { AuthProvider } from "./context/AuthContext";
 import { BlogProvider } from "./context/BlogContext";
+import { ThemeProvider, CSSReset, theme } from "@chakra-ui/core";
+
+// const customTheme = {
+//   ...theme,
+//   colors: {
+//     ...theme.colors,
+//     brand: {
+//       900: "#1a365d",
+//       800: "#153e75",
+//       700: "#2a69ac"
+//     }
+//   }
+// };
+
+const config = theme => ({
+  light: {
+    color: theme.colors.gray[700],
+    bg: theme.colors.gray[100],
+    borderColor: theme.colors.gray[200],
+    placeholderColor: theme.colors.gray[500]
+  },
+  dark: {
+    color: theme.colors.whiteAlpha[900],
+    bg: theme.colors.gray[800],
+    borderColor: theme.colors.whiteAlpha[300],
+    placeholderColor: theme.colors.whiteAlpha[400]
+  }
+});
 
 function App() {
   return (
-    <AuthProvider>
-      <BlogProvider>
-        <Router>
-          <Main />
-        </Router>
-      </BlogProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <CSSReset config={config} />
+      <AuthProvider>
+        <BlogProvider>
+          <Router>
+            <Main />
+          </Router>
+        </BlogProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
