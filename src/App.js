75355637ed2247c5c7613e736b22dc9577dd69fb
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-11-01 03:53:30 -0800

    Add breakpoints

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import { AuthProvider } from "./context/AuthContext";
 import { BlogProvider } from "./context/BlogContext";
 import { ThemeProvider, CSSReset, theme } from "@chakra-ui/core";
 
-// const customTheme = {
-//   ...theme,
-//   colors: {
-//     ...theme.colors,
-//     brand: {
-//       900: "#1a365d",
-//       800: "#153e75",
-//       700: "#2a69ac"
-//     }
-//   }
-// };
+const breakpoints = ["910px", "1050px", "1100px", "1440px"];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+const customTheme = {
+  ...theme,
+  breakpoints
+
+  // colors: {
+  //   ...theme.colors,
+  //   brand: {
+  //     900: "#1a365d",
+  //     800: "#153e75",
+  //     700: "#2a69ac"
+  //   }
+  // }
+};
 
 const config = theme => ({
   light: {
@@ -34,7 +42,7 @@ const config = theme => ({
 
 function App() {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={customTheme}>
       <CSSReset config={config} />
       <AuthProvider>
         <BlogProvider>
